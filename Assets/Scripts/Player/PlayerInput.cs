392a1428dee237f04bc0262d4274fbcdaec76ed0
minseo100518 <zizonehood10@gmail.com>
2026-07-09 17:41:08 +0900

    player movement

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private PlayerMovement movement;
+
+    public Vector2 axis;
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 axis_ = value.Get<Vector2>();
+        axis = new Vector2(axis_.x, 0);
+    }
+
+    public bool HasAxis()
+    {
+        return axis.x != 0 || axis.y != 0;
+    }
+    public void OnJump()
+    {
+        movement.Jump();
+    }
+}
